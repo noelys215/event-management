e3eb94d72aa7af753c550b3a6321f9b4e20343ac
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-07-16 22:25:56 -0400

    update migration and models

--- database/migrations/2024_07_16_020338_create_events_table.php
+++ database/migrations/2024_07_16_020338_create_events_table.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->string('name');
+            $table->text('description')->nullable();
+
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+
             $table->timestamps();
         });
     }
--- database/migrations/2024_07_16_020418_create_attendees_table.php
+++ database/migrations/2024_07_16_020418_create_attendees_table.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('attendees', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Event::class);
+
             $table->timestamps();
         });
     }
