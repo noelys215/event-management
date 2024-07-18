e217cfcb5ed29a46b5643290b235f3b60ff1a984
Henry Betancourth <69738617+noelys215@users.noreply.github.com>
2024-07-17 21:37:08 -0400

    update AttendeeSeeder.php

--- database/seeders/AttendeeSeeder.php
+++ database/seeders/AttendeeSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Attendee;
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,18 @@ class AttendeeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = User::all();
+        $events = Event::all();
+
+        foreach ($users as $user) {
+            $eventsToAttend = $events->random(rand(1, 3));
+
+            foreach ($eventsToAttend as $event) {
+                Attendee::create([
+                    'user_id' => $user->id,
+                    'event_id' => $event->id
+                ]);
+            }
+        }
     }
 }
